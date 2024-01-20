2d6a1c7689b698b3bb84c5358c39295a5266ba85
SadRus <rystixx@yandex.ru>
2024-01-20 23:45:58 +0300

    Add weather model

--- yandex_weather/models.py
+++ yandex_weather/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Weather(models.Model):
+    city = models.CharField('Город', max_length=100)
+    latitude = models.FloatField('Широта')
+    longitude = models.FloatField('Долгота')
+    temperature = models.FloatField(
+        'Текущая температура',
+        blank=True,
+        null=True,
+    )
+    pressure = models.FloatField(
+        'Давление в мм рт.ст.',
+        blank=True,
+        null=True,
+    )
+    wind_speed = models.FloatField(
+        'Скорость ветра',
+        blank=True,
+        null=True,
+    )
+    request_timestamp = models.IntegerField(
+        default=0,
+        blank=True,
+        null=True,
+    )
