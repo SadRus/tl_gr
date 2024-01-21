7378c1841b9327697b27cde3590086db7d6be8b7
SadRus <rystixx@yandex.ru>
2024-01-21 22:49:34 +0300

    Add APIView for /weather url

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from yandex_weather.views import WeatherAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('weather/', WeatherAPIView.as_view()),
 ]
--- yandex_weather/views.py
+++ yandex_weather/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Weather
+from .serializers import WeatherSerializer
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class WeatherAPIView(APIView):
+    def get(self, request):
+        city = request.query_params.get('city')
+        weather = Weather.objects.filter(city=city).first()
+        serializer_class = WeatherSerializer(weather)
+        return Response(serializer_class.data)
